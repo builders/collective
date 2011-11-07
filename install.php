d95b2c1ddf2265de9b83508f17957b4542207696
Stephen Bau <stephen@domain7.com>
2011-11-06 19:32:29 -0800

    Add organization workspace as a submodule and update installer files

--- install.php
+++ install.php
@@ -10,19 +10,18 @@
 	}
 
 	if(!defined('PHP_VERSION_ID')){
-		$version = PHP_VERSION;
-		define('PHP_VERSION_ID', ($version{0} * 10000 + $version{2} * 100 + $version{4}));
+    	$version = PHP_VERSION;
+    	define('PHP_VERSION_ID', ($version{0} * 10000 + $version{2} * 100 + $version{4}));
 	}
 
 	if (PHP_VERSION_ID >= 50300){
-		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+	    error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 	}
 	else{
-		error_reporting(E_ALL ^ E_NOTICE);
+	    error_reporting(E_ALL ^ E_NOTICE);
 	}
 
 	set_error_handler('__errorHandler');
-	ini_set('display_errors', 1);
 
 	// Show PHP Info
 	if(isset($_REQUEST['info'])){
@@ -37,7 +36,7 @@
 	}
 
 	// Defines
-	define('kVERSION', '2.2.5RC1');
+	define('kVERSION', '2.2.4');
 	define('kINSTALL_ASSET_LOCATION', './symphony/assets/installer');
 	define('kINSTALL_FILENAME', basename(__FILE__));
 	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
@@ -71,7 +70,7 @@
 	}
 
 	/***********************
-			 TESTS
+	         TESTS
 	************************/
 
 	// Check and set language
@@ -182,24 +181,55 @@
 		$conf['admin']['max_upload_size'] = '5242880';
 		$conf['symphony']['pagination_maximum_rows'] = '17';
 		$conf['symphony']['allow_page_subscription'] = '1';
-		$conf['symphony']['lang'] = Lang::get();
-		$conf['symphony']['version'] = kVERSION;
+		$conf['symphony']['lang'] = 'en';
 		$conf['symphony']['pages_table_nest_children'] = 'yes';
+		$conf['symphony']['version'] = '2.2.4';
 		$conf['symphony']['strict_error_handling'] = 'yes';
 		$conf['symphony']['session_gc_divisor'] = '10';
 		$conf['log']['archive'] = '1';
 		$conf['log']['maxsize'] = '102400';
-		$conf['general']['sitename'] = 'Symphony CMS';
 		$conf['image']['cache'] = '1';
 		$conf['image']['quality'] = '90';
 		$conf['database']['character_set'] = 'utf8';
 		$conf['database']['character_encoding'] = 'utf8';
 		$conf['database']['runtime_character_set_alter'] = '1';
-		$conf['database']['query_caching'] = 'on';
-		$conf['public']['display_event_xml_in_source'] = 'yes';
-		$conf['region']['time_format'] = 'H:i';
+		$conf['database']['query_caching'] = 'default';
+		$conf['public']['display_event_xml_in_source'] = 'no';
+		$conf['general']['sitename'] = 'Organization';
+		$conf['region']['time_format'] = 'g:i a';
 		$conf['region']['date_format'] = 'd F Y';
 		$conf['region']['datetime_separator'] = ' ';
+		$conf['maintenance_mode']['enabled'] = 'no';
+		$conf['dump_db']['path'] = '/workspace/sql';
+		$conf['dump_db']['restore'] = 'yes';
+		$conf['dump_db']['last_sync'] = '2011-11-06T15:00:48-08:00';
+		$conf['openid-auth']['store-path'] = '/Users/stephen/Sites/sym/org/manifest/openid';
+		$conf['openid-auth']['sreg-required-fields'] = '';
+		$conf['openid-auth']['sreg-optional-fields'] = '';
+		$conf['openid-auth']['login-redirect'] = '/dashboard/';
+		$conf['openid-auth']['logout-redirect'] = '/login/';
+		$conf['members']['cookie-prefix'] = 'sym-members';
+		$conf['members']['section'] = '1';
+		$conf['members']['reset-password-template'] = 'etm-reset-password';
+		$conf['members']['regenerate-activation-code-template'] = 'etm-regenerate-activation-code';
+		$conf['members']['generate-recovery-code-template'] = 'etm-reset-password';
+		$conf['members']['activate-account-template'] = '';
+		$conf['migrations']['enabled'] = 'yes';
+		$conf['forum']['discussion-section'] = '2';
+		$conf['forum']['comment-section'] = '3';
+		$conf['forum']['created-by-field'] = '154';
+		$conf['forum']['discussion-last-post-field'] = '155';
+		$conf['forum']['discussion-last-active-field'] = '14';
+		$conf['forum']['unread-cutoff-field'] = '19';
+		$conf['forum']['pinned-field'] = '15';
+		$conf['forum']['locked-field'] = '16';
+		$conf['forum']['comment-discussion-link-field'] = '18';
+		$conf['forum']['comment-created-by-field'] = '157';
+		$conf['forum']['comment-creation-date-field'] = '19';
+		$conf['forum']['comment-field'] = '68';
+		$conf['content-type-mappings']['xml'] = 'text/xml; charset=utf-8';
+		$conf['content-type-mappings']['text'] = 'text/plain; charset=utf-8';
+		$conf['readonly_mode']['enabled'] = 'no';
 
 		return $conf;
 
